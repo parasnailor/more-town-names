7508232e81997b58fdd92d4a674f48b9cd12f0ea
wes <wesk1996@gmail.com>
2026-05-04 00:32:04 -0400

    remove unnecessary namespace

--- NameProvider.cs
+++ NameProvider.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MoreTownNames;
 
-namespace TownNamesMod
+namespace MoreTownNames
 {
     public static class NameProvider
     {
--- WorldStateServicePatch.cs
+++ WorldStateServicePatch.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using HarmonyLib;
 using Eremite.Services;
-using TownNamesMod;
 using System.Linq;
 
 namespace MoreTownNames.Patches;
